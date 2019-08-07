8cb3a15bea640c71d81a0729693005e0bb97f2bf
Krystal Campbell <krystalcampbell@Krystals-MacBook-Pro.local>
2019-08-07 14:19:20 -0400

    first commit

---
+++ LabAutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LabAutoLayout
+//
+//  Created by Krystal Campbell on 8/7/19.
+//  Copyright © 2019 Krystal Campbell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
